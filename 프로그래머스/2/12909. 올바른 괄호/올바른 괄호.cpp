3e8e513627a41c6f68cc20e9621306b2c8cfacaf
김건호 <128780883+kimgho@users.noreply.github.com>
2023-11-03 03:01:08 +0900

    [level 2] Title: 올바른 괄호, Time: 0.57 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,24 @@
+#include<string>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> st;
+    for(char& c: s){
+        if(c=='(')
+            st.push(c);
+        else if(c==')'){
+            if(!st.empty()){
+                st.pop();
+            }
+            else{
+                st.push(')');
+            }
+        }
+    }
+    if(!st.empty()) answer =false;
+    return answer;
+}
