406a3e00800dcfab0e877b9ec379044ca8e2baf6
김건호 <128780883+kimgho@users.noreply.github.com>
2023-11-28 02:21:08 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.46 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -1,31 +1,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
-
-bool compare(const vector<int>& a, const vector<int>& b, int idx) {
-    return a[idx] < b[idx];
+int sortIdx =0;
+bool compare(const vector<int> a, const vector<int> b) {
+    return a[sortIdx] < b[sortIdx];
 }
 
 vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
     vector<vector<int>> answer;
     vector<string> s = {"code","date","maximum","remain"};
-    int extIdx = 0,sortIdx = 0;
+    int idx =0;
     for(int i=0;i<4;i++){
         if(ext==s[i]){
-            extIdx = i;
-        }
-        if(sort_by ==s[i]){
-            sortIdx =i;
+            sortIdx = i;
         }
     }
-    int met = sortIdx;
     for(int i=0;i<data.size();i++){
-        if(data[i][extIdx]<val_ext)
+        if(data[i][sortIdx]<val_ext)
             answer.push_back(data[i]);
     }
-    sort(answer.begin(), answer.end(), [met](const vector<int>& a, const vector<int>& b) {
-        return compare(a, b, met);
-    });
+    for(int i=0;i<s.size();i++){
+        if(sort_by==s[i]){
+            sortIdx = i;
+        }
+    }
+    sort(answer.begin(), answer.end(), compare);
     return answer;
 }
