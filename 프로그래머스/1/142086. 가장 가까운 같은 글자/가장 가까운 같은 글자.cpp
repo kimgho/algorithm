8945b625ca8a9527d1c64d213a8390888fcb4bc2
김건호 <128780883+kimgho@users.noreply.github.com>
2023-12-25 00:19:06 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 17.80 ms, Memory: 5.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    int idx;
+    for(int i=0;i<s.size();i++){
+        idx =-1;
+        for(int j=0;j<i;j++){
+            if(s[i]==s[j]){
+                idx =i-j;
+            }   
+        }
+        answer.push_back(idx);
+    }
+    return answer;
+}
