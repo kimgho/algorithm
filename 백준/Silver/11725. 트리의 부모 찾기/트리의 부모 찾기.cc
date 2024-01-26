6a14565c6ebb11892adffb9f36f7fd114b70004d
김건호 <128780883+kimgho@users.noreply.github.com>
2024-01-26 20:13:58 +0900

    bfs,dfs 두 가지 추가

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 #define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define MAX 100001
@@ -7,19 +8,40 @@ int n;
 vector<int> tr[MAX];
 int arr[MAX];
 bool visited[MAX];
-void dfs(int n)
+queue<int> q;
+void bfs(int n)
 {
     visited[n] = true;
-    for (int i = 0; i < tr[n].size(); i++)
+    q.push(n);
+    while (!q.empty())
     {
-        int x = tr[n][i];
-        if (visited[x])
-            continue;
-        visited[x] = true;
-        arr[x] = n;
-        dfs(x);
+        int x = q.front();
+        q.pop();
+        for (int i = 0; i < tr[n].size(); i++)
+        {
+            int next = tr[n][i];
+            if (!visited[next])
+            {
+                arr[next] = n;
+                q.push(next);
+                bfs(next);
+            }
+        }
     }
 }
+// void dfs(int n)
+// {
+//     visited[n] = true;
+//     for (int i = 0; i < tr[n].size(); i++)
+//     {
+//         int x = tr[n][i];
+//         if (visited[x])
+//             continue;
+//         visited[x] = true;
+//         arr[x] = n;
+//         dfs(x);
+//     }
+// }
 int main()
 {
     io;
@@ -31,7 +53,8 @@ int main()
         tr[a].push_back(b);
         tr[b].push_back(a);
     }
-    dfs(1);
+    // dfs(1);
+    bfs(1);
     for (int i = 2; i <= n; i++)
     {
         cout << arr[i] << "\n";
