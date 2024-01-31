39b59f3b7c62df346eaa05cabbb3b4ac622ba924
김건호 <128780883+kimgho@users.noreply.github.com>
2024-01-31 20:48:23 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 40 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define io ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX 999999999
+int n;
+int mat[501][2];
+int dp[501][501];
+int main()
+{
+    io;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        mat[i][0] = a;
+        mat[i][1] = b;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i][i] = 0;
+    }
+    for (int l = 2; l <= n; l++)
+    {
+        for (int i = 1; i <= n - l + 1; i++)
+        {
+            int j = i + l - 1;
+            dp[i][j] = MAX;
+            for (int k = i; k <= j - 1; k++)
+            {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + mat[i][0] * mat[k][1] * mat[j][1]);
+            }
+        }
+    }
+    cout << dp[1][n];
+    return 0;
+}
